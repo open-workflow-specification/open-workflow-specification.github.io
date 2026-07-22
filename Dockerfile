2e8e4ee0ae65f34b160cf513085f1544da1d4cc0
Ricardo Zanini <1538000+ricardozanini@users.noreply.github.com>
2026-07-22 12:30:38 -0400

    Replace Astro site with minimal static redirect page (#268)
    Keep schemas served locally for backward compatibility.
    Everything else redirects to open-workflow-specification.org.
    
    Signed-off-by: Ricardo Zanini <ricardozanini@gmail.com>

--- Dockerfile
+++
@@ -1,7 +0,0 @@
-FROM node:lts
-WORKDIR /app
-COPY . .
-RUN npm i
-RUN npx astro telemetry disable
-EXPOSE 4321
-CMD ["npx", "astro", "dev", "--host"]
--- src/utils/collections.ts
+++
@@ -1,6 +0,0 @@
-import { getCollection } from "astro:content";
-
-export const getSortedBlogPosts = async () => 
-  (await getCollection('blog'))
-    .sort((a, b) => b.data.date.valueOf() - a.data.date.valueOf())
-    ;
